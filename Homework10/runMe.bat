297d469ee040ef2a023b22b520250452813fde3a
simon1010 <simon.szabolcs10@gmail.com>
2016-03-16 23:41:27 +0200

    new changes

--- Homework10/runMe.bat
+++ Homework10/runMe.bat
@@ -1,5 +1,5 @@
 cd bin
-start java com.atraxo.homework10.Ex3.ChatServer
-start java com.atraxo.homework10.Ex3.ChatClient
-start java com.atraxo.homework10.Ex3.ChatClient
-start java com.atraxo.homework10.Ex3.ChatClient
+start "Chat Server" java com.atraxo.homework10.Ex3.ChatServer
+start "Client 1" java com.atraxo.homework10.Ex3.ChatClient
+start "Client 2" java com.atraxo.homework10.Ex3.ChatClient
+start "Client 3" java com.atraxo.homework10.Ex3.ChatClient
--- Homework8/src/com/atraxo/homework8/ModRestaurant.java
+++ Homework8/src/com/atraxo/homework8/ModRestaurant.java
@@ -3,25 +3,42 @@ package com.atraxo.homework8;
 import static net.mindview.util.Print.print;
 import static net.mindview.util.Print.printnb;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class ModRestaurant {
 	ModMeal meal;
-	ExecutorService exec = Executors.newCachedThreadPool();
-	ModWaitPerson waitPerson = new ModWaitPerson(this);
-	ModWaitPerson otherPerson = new ModWaitPerson(this);
-	ModChef chef = new ModChef(this);
+	List<ModWaitPerson> waitPerson = new ArrayList<>();
+	List<ModChef> chef = new ArrayList<>();
 
-	public ModRestaurant() {
-		exec.execute(chef);
-		exec.execute(waitPerson);
-		exec.execute(otherPerson);
+	ExecutorService exec;
+
+	BlockingQueue<Integer> incoming;
+	BlockingQueue<Integer> outgoing;
+
+	public ModRestaurant(int nrWaitPersons, int nrChefs) {
+		exec = Executors.newFixedThreadPool(nrChefs + nrWaitPersons);
+
+		incoming = new ArrayBlockingQueue<>(nrWaitPersons);
+		outgoing = new ArrayBlockingQueue<>(nrWaitPersons);
+		
+		for (int i = 0; i < nrWaitPersons; i++)
+			waitPerson.add(new ModWaitPerson(this, i));
+		for (int i = 0; i < nrChefs; i++)
+			chef.add(new ModChef(this, i));
+		for (ModWaitPerson wp : waitPerson)
+			exec.execute(wp);
+		for (ModChef cf : chef)
+			exec.execute(cf);
 	}
 
 	public static void main(String[] args) {
-		new ModRestaurant();
+		new ModRestaurant(4, 4);
 	}
 
 	class ModMeal {
@@ -38,22 +55,25 @@ public class ModRestaurant {
 
 	class ModWaitPerson implements Runnable {
 		private ModRestaurant restaurant;
+		public int id;
 
-		public ModWaitPerson(ModRestaurant r) {
+		public ModWaitPerson(ModRestaurant r, int id) {
 			restaurant = r;
+			this.id = id;
 		}
 
 		public void run() {
 			try {
 				while (!Thread.interrupted()) {
+					Thread.sleep(1000);
 					synchronized (this) {
 						while (restaurant.meal == null)
 							wait(); // ... for the chef to produce a meal
 					}
-					print("Waitperson got " + restaurant.meal);
-					synchronized (restaurant.meal) {
+					print(this.id + ": Waitperson got " + restaurant.meal + "\n");
+					synchronized (restaurant.chef.get(id)) {
 						restaurant.meal = null;
-						restaurant.chef.notifyAll(); // Ready for another
+						restaurant.chef.get(id).notify(); // Ready for another
 					}
 				}
 			} catch (InterruptedException e) {
@@ -65,40 +85,34 @@ public class ModRestaurant {
 	class ModChef implements Runnable {
 
 		private ModRestaurant restaurant;
-		private int count = 0;
-
-		public ModChef(ModRestaurant r) {
+		public int id;
+		int count = 0;
+		
+		public ModChef(ModRestaurant r, int id) {
 			restaurant = r;
+			this.id = id;
 		}
 
 		public void run() {
 			
-			System.out.println(restaurant.waitPerson);
-			System.out.println(restaurant.otherPerson);
 			
 			try {
 				while (!Thread.interrupted()) {
+					Thread.sleep(1000);
 					synchronized (this) {
 						while (restaurant.meal != null)
 							wait(); // ... for the meal to be taken
 					}
-					if (++count == 3) {
-						print("Out of food, closing");
-						restaurant.exec.shutdownNow();
-					}
-					printnb("Order up! ");
-					synchronized (restaurant.waitPerson) {
+						if (++count == 10) {
+							print("Out of food, closing");
+							restaurant.exec.shutdownNow();
+						}
+					printnb(this.id + ": Order up! \n");
+					synchronized (restaurant.waitPerson.get(id)) {
 						restaurant.meal = new ModMeal(count);
-						restaurant.waitPerson.notifyAll();
-//						restaurant.otherPerson.notifyAll();
+						restaurant.waitPerson.get(id).notify();
 					}
-					
-//					synchronized (restaurant.otherPerson) {
-//						restaurant.meal = new ModMeal(count);
-////						restaurant.waitPerson.notifyAll();
-//						 restaurant.otherPerson.notifyAll();
-//					}
-					
+
 					TimeUnit.MILLISECONDS.sleep(100);
 				}
 			} catch (InterruptedException e) {
